94f28c47967d3834c2c202bd981e91bea3f3b5d5
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-09-24 13:30:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const statusParagraph = document.getElementById("status");
+const enterButton = document.getElementById("enterBtn");
+const displayText = document.getElementById("displayText");
+
+enterButton.addEventListener("click", function() {
+    statusParagraph.textContent = "Entered Metaverse";
+    
+    displayText.textContent = statusParagraph.textContent;
+});
