6a7c5f1db011afc94d951f2dbf27da3a380762ec
PrGhostDev <139947007+PrGhostDev@users.noreply.github.com>
2023-09-29 15:32:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 //your JS code here. If required.
 const statusParagraph = document.getElementById("status");
 const enterButton = document.getElementById("enterBtn");
-const displayText = document.getElementById("displayText");
 
-enterButton.addEventListener("click", function() {
-    statusParagraph.textContent = "Entered Metaverse";
+enterButton.addEventListener("click", function () {
+    const newHeading = document.createElement("h1");
+    newHeading.textContent = "Entered Metaverse";
     
-    displayText.textContent = statusParagraph.textContent;
+    // Replace the old <p> with the new <h1>
+    statusParagraph.parentNode.replaceChild(newHeading, statusParagraph);
 });
